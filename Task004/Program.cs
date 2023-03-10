631e9072015e98400a14a66afbc7304b422dfcb7
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-10 21:36:20 +0300

    add task004 Seminar 2

---
+++ Task004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = 7;
+int b = 23;
+int x1 = num%a;
+int x2 = num%b;
+if (x1==0 && x2==0)
+{
+    Console.WriteLine("да, кратно обоим");
+}
+else 
+{
+    Console.WriteLine("не кратно обоим");
+}
